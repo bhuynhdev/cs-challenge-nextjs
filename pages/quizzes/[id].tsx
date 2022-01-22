a99df0b5a4e7153371d9832d8753a2449221062b
baohuynhlam <shinsusenju@aol.com>
2022-01-22 10:55:40 -0500

    Format code with Prettier

--- pages/quizzes/[id].tsx
+++ pages/quizzes/[id].tsx
@@ -1,45 +1,42 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import { ParsedUrlQuery } from "querystring";
-import { QuizQuestionData, data } from "../../quizData"
-import styles from "../../styles/Quiz.module.css"
-import { FormEvent, useEffect, useRef, useState } from "react"
+import { QuizQuestionData, data } from "../../quizData";
+import styles from "../../styles/Quiz.module.css";
+import { FormEvent, useRef, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 interface Params extends ParsedUrlQuery {
-    id: string,
+    id: string;
 }
 
 interface Props {
-    quizData: QuizQuestionData
+    quizData: QuizQuestionData;
 }
 
 export default function Quiz({ quizData }: Props) {
     const { query } = useRouter();
     const id = query.id as string;
     const { title, prose, images, choices, answerIndex, hint, explanation } = quizData;
-    const choicesRefs = useRef<HTMLButtonElement[]>([])
-    const [playerChoice, setPlayerChoice] = useState("")
+    const choicesRefs = useRef<HTMLButtonElement[]>([]);
+    const [playerChoice, setPlayerChoice] = useState("");
     const [hasWon, setHasWon] = useState(false);
     const [shouldShowCaptions, setShouldShowCaptions] = useState(false);
     const [shouldShowHint, setShouldShowHint] = useState(false);
 
-    useEffect(() => {
-        setHasWon(false);
-    }, [id]);
-
     const chooseChoice = (buttonIndex: number) => {
         const textOfButton = choicesRefs.current[buttonIndex].innerText;
         setPlayerChoice(textOfButton);
-    }
+    };
 
     const showCaptions = () => {
         setShouldShowCaptions(true);
-    }
+    };
 
     const showHint = () => {
         setShouldShowHint(true);
-    }
+    };
+
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (playerChoice === choices[answerIndex]) {
@@ -48,7 +45,7 @@ export default function Quiz({ quizData }: Props) {
         } else {
             alert("WRONG");
         }
-    }
+    };
     return (
         <div className={styles.container}>
             <h1>{title}</h1>
@@ -60,43 +57,73 @@ export default function Quiz({ quizData }: Props) {
                             return (
                                 <figure key={i} className={styles.figure}>
                                     <img src={url}></img>
-                                    <figcaption>Pic {i + 1} {shouldShowCaptions && <span>: {text}</span>}</figcaption>
-                                </figure>)
+                                    <figcaption>
+                                        Pic {i + 1} {shouldShowCaptions && <span>: {text}</span>}
+                                    </figcaption>
+                                </figure>
+                            );
                         })}
                     </div>
-                    <hr/>
-                    <p>The answer choices does not exactly correspond 1-1 to above pictures. Sorry for the visual confusion</p>
+                    <hr />
+                    <p>
+                        The answer choices does not exactly correspond 1-1 to above pictures. Sorry
+                        for the visual confusion
+                    </p>
                     <div className={`${styles.table22} ${styles.choices}`}>
                         {choices.map((choice, i) => {
-                            return <button type="button" key={i} onClick={() => chooseChoice(i)} ref={el => { choicesRefs.current[i] = el! }}>{choice}</button>
+                            return (
+                                <button
+                                    type="button"
+                                    key={i}
+                                    onClick={() => chooseChoice(i)}
+                                    ref={(el) => {
+                                        choicesRefs.current[i] = el!;
+                                    }}
+                                >
+                                    {choice}
+                                </button>
+                            );
                         })}
                     </div>
                 </div>
                 {shouldShowHint && <p>{hint ? hint : "No more hints to give"}</p>}
-                {hasWon && <p>{`Explanation: ${explanation}`}</p>}                
-                {!hasWon && <button type="submit" className={styles.submitButton}>SUBMIT</button>}
+                {hasWon && <p>{`Explanation: ${explanation}`}</p>}
+                {!hasWon && (
+                    <button type="submit" className={styles.submitButton}>
+                        SUBMIT
+                    </button>
+                )}
             </form>
-            {hasWon && parseInt(id) <= 4 && <Link href={`/quizzes/${parseInt(id) + 1}`}><button>NEXT</button></Link>}
+            {hasWon && parseInt(id) <= 4 && (
+                <Link href={`/quizzes/${parseInt(id) + 1}`}>
+                    <button>NEXT</button>
+                </Link>
+            )}
             <div>
-                <button type="button" onClick={showCaptions}>Hint 1: Show image captions</button>
-                <button type="button" onClick={showHint}>Hint 2: Even more suggestions</button>
+                <button type="button" onClick={showCaptions}>
+                    Hint 1: Show image captions
+                </button>
+                <button type="button" onClick={showHint}>
+                    Hint 2: Even more suggestions
+                </button>
             </div>
         </div>
-    )
+    );
 }
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
-    const paths = data.map((obj, i) => {
-        return { params: { id: (i + 1).toString() } }
-    })
-    return { paths, fallback: false }
-}
+    const paths = data.map((_, i) => {
+        return { params: { id: (i + 1).toString() } };
+    });
+    return { paths, fallback: false };
+};
 
 export const getStaticProps: GetStaticProps<Props, Params> = async ({ params }) => {
     const id = params!.id;
     return {
         props: {
+            key: id,
             quizData: data[parseInt(id) - 1],
-        }
-    }
-}
+        },
+    };
+};
